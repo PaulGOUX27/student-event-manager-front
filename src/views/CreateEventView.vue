d16d29813f212a0c3747f9d3522ce6ce8b6cf834
PaulGOUX27 <paul.goux@wanadoo.fr>
2020-10-13 14:45:23 +0300

    edit view names 2

--- src/views/CreateEventView.vue
+++ src/views/CreateEventView.vue
@@ -6,7 +6,7 @@
 
 <script>
 export default {
-  name: 'CreateEvent',
+  name: 'CreateEventView',
 };
 </script>
 
--- src/views/EventListView.vue
+++ src/views/EventListView.vue
@@ -9,7 +9,7 @@
 
 <script>
 export default {
-  name: 'EventList',
+  name: 'EventListView',
 };
 </script>
 
