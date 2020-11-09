664c533d047c41c0505b194fe2f6c4963bc27e63
PaulGOUX27 <paul.goux@wanadoo.fr>
2020-11-09 23:00:32 +0200

    Create EventCategorySelector component

---
+++ src/components/EventCategorySelector.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <multiselect v-model="selectedEventCategories" :options="eventCategories"
+                 label="name" track-by="id" :allow-empty="true" :multiple="true"
+                 :taggable="true" @tag="addEventCategory" @input="newEventCategory"/>
+  </div>
+</template>
+
+<script>
+import Multiselect from 'vue-multiselect';
+import EventCategoryService from '@/services/EventCategoryService';
+
+export default {
+  name: 'EventCategorySelector',
+  components: {
+    Multiselect,
+  },
+  data() {
+    return {
+      eventCategories: [],
+      selectedEventCategories: [],
+    };
+  },
+  mounted() {
+    EventCategoryService.getAll()
+      .then((response) => { this.eventCategories = response.result['event-categories']; });
+  },
+  methods: {
+    addEventCategory(name) {
+      EventCategoryService.create({ name })
+        .then((response) => {
+          const newEventCategory = response.result;
+          this.eventCategories.push(newEventCategory);
+          this.selectedEventCategories.push(newEventCategory);
+        });
+    },
+    newEventCategory() {
+      this.$emit('input', this.selectedEventCategories);
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
