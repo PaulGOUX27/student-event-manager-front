2ed257eb8ba9aa70c61dc27f55638cf7c894832a
PaulGOUX27 <paul.goux@wanadoo.fr>
2020-11-12 14:55:42 +0200

    Style improvement

--- src/views/EventListView.vue
+++ src/views/EventListView.vue
@@ -1,8 +1,18 @@
 <template>
-  <div>
-    <div v-for="e in events" :key="e.id">
-      {{e.title}}
-      <router-link :to="`/edit/${e.id}`" tag="button">
+  <div class="container">
+    <div v-for="e in events" :key="e.id" class="event-list">
+      <div class="event-info">
+        <div>
+          {{e.title}}
+        </div>
+        <div>
+          {{e.place}}
+        </div>
+        <div>
+          {{e.start_date}}
+        </div>
+      </div>
+      <router-link :to="`/edit/${e.id}`" tag="button" class="green-button">
         Edit
       </router-link>
     </div>
@@ -29,5 +39,20 @@ export default {
 </script>
 
 <style scoped>
+.event-list{
+  display: flex;
+  justify-content: space-between;
+  width: 800px;
+  margin-bottom: 15px;
+  align-items: center;
+}
 
+.event-info{
+  display: flex;
+  justify-content: space-evenly;
+}
+
+.event-info,div{
+  margin-right: 40px;
+}
 </style>
