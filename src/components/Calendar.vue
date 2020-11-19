4b0b3462ce405631716b0113d9ef4138d47aa74c
PaulGOUX27 <paul.goux@wanadoo.fr>
2020-11-19 11:50:47 +0200

    Fix calendar style

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -4,7 +4,13 @@
            :hide-view-selector="true"
            :time-from="0" :time-to="24 * 60" :time-step="60"
            :events="eventsToCalendar"
-  />
+  >
+    <template v-slot:time-cell="{ hours }">
+      <div>
+        <span style="color: black;">{{ hours }}:00</span>
+      </div>
+    </template>
+  </vue-cal>
 </template>
 
 <script>
@@ -48,6 +54,10 @@ export default {
   height: 500px;
 }
 
+.vuecal__menu {
+  color: black;
+}
+
 .vuecal__event.tik {
   background-color: rgba(253, 156, 66, 0.9);
   border: 1px solid rgb(233, 136, 46);
@@ -57,5 +67,17 @@ export default {
   background-color: rgba(255, 102, 102, 0.9);
   border: 1px solid rgb(235, 82, 82);
   color: #fff;
+}
+
+.vuecal__weekdays-headings {
+  color: black;
+}
+
+.vuecal--view-with-time {
+  color: black;
+}
+
+.vuecal__no-event {
+  color: black;
 }
 </style>
